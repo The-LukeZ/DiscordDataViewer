e2c722a6d9d620df4dec621361e56dad2fe1999c
The-LukeZ <luke.hent3005@gmail.com>
2025-07-14 21:46:14 +0200

    fix: change response status from 401 to 410 for unauthorized API access

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -32,7 +32,7 @@ const _handle: Handle = async ({ event, resolve }) => {
 
   if (event.url.pathname.startsWith("/api/") && event.url.pathname !== "/api/callback") {
     if (!event.locals.loggedIn) {
-      return new Response(null, { status: 401, statusText: "Unauthorized" });
+      return new Response(null, { status: 410, statusText: "Gone" });
     }
   }
 
