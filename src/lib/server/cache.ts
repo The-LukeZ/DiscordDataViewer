a25ab066fa6cd5c07a662a29a0aad674c5b275ad
The-LukeZ <luke.hent3005@gmail.com>
2025-07-14 20:54:05 +0200

    feat: replace NodeCache with Cloudflare KV for session management

--- src/lib/server/cache.ts
+++ src/lib/server/cache.ts
@@ -1,18 +1,52 @@
 import type { RESTPostOAuth2AccessTokenResult } from "discord-api-types/v10";
-import NodeCache from "node-cache";
+import Cloudflare from "cloudflare";
+import { env } from "$env/dynamic/private";
 
-const cache = new NodeCache({
-  stdTTL: 3600,
-  checkperiod: 120,
-  errorOnMissing: false,
+const cf = new Cloudflare({
+  apiEmail: env.CLOUDFLARE_EMAIL,
+  apiToken: env.CLOUDFLARE_API_TOKEN,
 });
 
-function getSession(token: string) {
-  return cache.get<RESTPostOAuth2AccessTokenResult>(token);
+type CloudflareKVJSONResponse = {
+  value: string;
+  metadata: {};
+};
+
+async function getSession(token: string) {
+  const res = await cf.kv.namespaces.values.get(env.KV_NAMESPACE_ID, token, {
+    account_id: env.CLOUDFLARE_ACCOUNT_ID,
+  });
+
+  const content = await res.blob();
+  const text = await content.text();
+
+  try {
+    const jsonWithStrings = JSON.parse(text);
+    return JSON.parse(jsonWithStrings.value) as RESTPostOAuth2AccessTokenResult;
+  } catch (error) {
+    console.error("Failed to parse JSON:", error);
+    return null;
+  }
 }
 
-function setSession(token: string, data: RESTPostOAuth2AccessTokenResult) {
-  cache.set(token, data);
+async function setSession(token: string, data: RESTPostOAuth2AccessTokenResult) {
+  const res = await cf.kv.namespaces.values.update(env.KV_NAMESPACE_ID, token, {
+    account_id: env.CLOUDFLARE_ACCOUNT_ID,
+    value: JSON.stringify(data),
+    expiration_ttl: 3600,
+    // Basically extra data to be attached to the KV entry that you can see in a list call if you need to
+    metadata: "",
+  });
+
+  return !!res;
 }
 
-export { getSession, setSession };
+async function deleteSession(token: string) {
+  const res = await cf.kv.namespaces.values.delete(env.KV_NAMESPACE_ID, token, {
+    account_id: env.CLOUDFLARE_ACCOUNT_ID,
+  });
+
+  return !!res;
+}
+
+export { getSession, setSession, deleteSession };
