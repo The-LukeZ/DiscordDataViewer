ea4ab24b7efc23397cf4804a96f8297893a55b60
The-LukeZ <luke.hent3005@gmail.com>
2025-07-14 21:54:50 +0200

    feat: add tunnel configuration for Sentry initialization

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -3,6 +3,7 @@ import * as Sentry from "@sentry/sveltekit";
 
 Sentry.init({
   dsn: "https://2564b960a14c382a2dc38b48edeed802@o4508704165265408.ingest.de.sentry.io/4509663839518800",
+  tunnel: "/tunnel",
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
