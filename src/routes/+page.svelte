1a46e03972abd01342b88d9b719e26038837282c
The-LukeZ <luke.hent3005@gmail.com>
2025-07-14 20:54:16 +0200

    fix: simplify layout structure for login form in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,37 +6,35 @@
   let loading = $state(false);
 </script>
 
-<div class="mx-auto min-h-screen w-full max-w-[900px] space-y-5">
-  {#if !data.loggedIn}
-    <div class="grid h-screen place-items-center">
-      <div class="card bg-base-300 w-96 shadow-sm">
-        <div class="card-body items-center">
-          <h2 class="card-title">Log in with Discord</h2>
-          <form
-            action="?/login"
-            method="POST"
-            use:enhance={() => {
-              loading = true;
-              return async ({ update, result }) => {
-                console.log("Form submitted", result);
-                await update();
-                if (result.type === "success" && result.data) {
-                  open(result.data.url as string, "_self");
-                } else {
-                  console.error("Error during login");
-                }
-                loading = false;
-              };
-            }}
+{#if !data.loggedIn}
+  <div class="grid h-screen place-items-center">
+    <div class="card bg-base-300 w-96 shadow-sm">
+      <div class="card-body items-center">
+        <h2 class="card-title">Log in with Discord</h2>
+        <form
+          action="?/login"
+          method="POST"
+          use:enhance={() => {
+            loading = true;
+            return async ({ update, result }) => {
+              console.log("Form submitted", result);
+              await update();
+              if (result.type === "success" && result.data) {
+                open(result.data.url as string, "_self");
+              } else {
+                console.error("Error during login");
+              }
+              loading = false;
+            };
+          }}
+        >
+          <button type="submit" class="btn btn-primary" disabled={loading}
+            >Log in with Discord</button
           >
-            <button type="submit" class="btn btn-primary" disabled={loading}
-              >Log in with Discord</button
-            >
-          </form>
-        </div>
+        </form>
       </div>
     </div>
-  {:else}
-    <DataViewer />
-  {/if}
-</div>
+  </div>
+{:else}
+  <DataViewer />
+{/if}
